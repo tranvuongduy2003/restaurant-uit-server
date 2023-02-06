09728c87afa51e739f1f590e96f7981d09e5af36
tranvuongduy2003 <tranvuongduy2003@gmail.com>
2023-02-06 22:54:27 +0700

    push code

--- models/order.js
+++ models/order.js
@@ -39,7 +39,7 @@ const orderSchema = new Schema(
       {
         id: {
           type: Schema.Types.ObjectId,
-          ref: 'Category',
+          ref: 'Food',
           require: false,
         },
         name: {
